fbc8b60daa89eb49e42fb5da2e547f84e764eb0c
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2024-02-05 08:41:58 -0500

    fetch movie api

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,40 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import {MovieCard} from "../movie-card/movie-card";
 import {MovieView} from "../movie-view/movie-view";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
 
+
+
 const [selectedMovie, setSelectedMovie] = useState(null);
 
 useEffect(() => {
-    fetch("https://mikes-movie-flix-5278ac249606.herokuapp.com/movies");
-});
+    fetch("https://mikes-movie-flix-5278ac249606.herokuapp.com/movies")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        const moviesFromApi = data.map((movie) => {
+            return {
+                id: movie._id,
+                title: movie.Title,
+                image: movie.imagePath,
+                description: movie.Description,
+                genre: {
+                    name: movie.Genre.Name,
+                    description: movie.Genre.Description
+                },
+                director: {
+                    name: movie.Director.Name,
+                    bio: movie.Director.Bio
+                },
+                featured: movie.Featured
+
+            };
+        });
+        setMovies(moviesFromApi);
+    });
+}, []);
 
 if (selectedMovie) {
     return (
