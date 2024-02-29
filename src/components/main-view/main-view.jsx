a48f27a31b5e8c930bb0836969e4b65b6b2c9155
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2024-02-29 09:22:59 -0500

    Fix bug with main-view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,15 +6,20 @@ import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import { Row } from "react-bootstrap";
 import { Col } from "react-bootstrap";
-import{ BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+import{ BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
     const [movies, setMovies] = useState([]);
     const [user, setUser] = useState(null);
+    const [token, setToken] = useState(storedToken? storedToken: null);
     
 
     useEffect(() => {
-        
+        if (!token) {
+            return;
+        }
         fetch("https://mikes-movie-flix-5278ac249606.herokuapp.com/movies", {
             headers: {Authorization: `Bearer ${token}`},
         })
@@ -69,7 +74,12 @@ export const MainView = () => {
                                     <Navigate to ="/" />
                                 ): (
                                     <Col md={5}>
-                                        <LoginView onLoggedIn={(user) => setUser(user)} />
+                                        <LoginView 
+                                            onLoggedIn={(user, token) => {
+                                                setUser(user);
+                                                setToken(token);
+                                            }}
+                                            />
                                     </Col>
                                 )}
                                 </>
