dc0fca8280793ca98897ebadd6327c020c03763e
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2024-02-29 08:48:25 -0500

    Updated movie-card with react

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,17 +1,18 @@
+import React from "react";
 import PropTypes from "prop-types";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import {Button, Card} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
-export const MovieCard = ({movie, onMovieClick}) => {
+export const MovieCard = ({movie}) => {
     return(
-        <Card className="h-100">
+        <Card>
             <Card.Img variant="top" src={movie.ImagePath} />
             <Card.Body>
                 <Card.Title>{movie.Title}</Card.Title>
                 <Card.Text>{movie.Director.Name}</Card.Text>
-                <Button onClick={() => onMovieClick(movie)} variant="link">
-                    Open
-                </Button>
+                <Link  to={`/movies/${encodeURIComponent(movie.id)}`}>
+                    <Button variant="link">Open</Button> 
+                </Link>
             </Card.Body>
         </Card>
     );
@@ -20,6 +21,7 @@ export const MovieCard = ({movie, onMovieClick}) => {
 MovieCard.propTypes = {
     movie: PropTypes.shape({
         Title: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired,
+        Director: PropTypes.string.isRequired
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired,
 };
