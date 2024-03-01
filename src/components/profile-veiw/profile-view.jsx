eaef4612c084e7b82c4a13133b31591f4dc08ebb
mikeb-molina <142452320+mikeb-molina@users.noreply.github.com>
2024-03-01 10:03:15 -0500

    Update profile-view with card

--- src/components/profile-veiw/profile-view.jsx
+++ src/components/profile-veiw/profile-view.jsx
@@ -35,5 +35,65 @@ export const ProfileView = ({user, token, movies, setUser}) => {
                 alert("Update failed")
             }
         });
-    }
+    };
+
+    return (
+        <Container className="my-5">
+            <Row>
+                <Col md={5}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>My Profile</Card.Title>
+                            <Card.Img variant="top" src="" className="w-50"/>
+                            <Card.Text>Username: {user.Username}</Card.Text>
+                            <Card.Text>Email: {user.Email}</Card.Text>
+                            <Card.Text>Birthday: {user.Birthday}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={7}>
+                    <Form onSubmit={handleupdate}>
+                        <Form.Group controlId="formUsername">
+                            <Form.Label>Username:</Form.Label>
+                            <Form.Control
+                            type="text"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            minLength="3"
+                            placeholder={user.Username}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formPassword">
+                            <Form.Label>Password:</Form.Label>
+                            <Form.Control
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            placeholder="******"
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formEmail">
+                            <Form.Label>Email:</Form.Label>
+                            <Form.Control
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            placeholder={user.Email}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formBirthday">
+                            <Form.Label>Birthday:</Form.Label>
+                            <Form.Control
+                            type="date"
+                            value={birthday}
+                            onChange={(e) => setBirthday(e.target.value)}
+                            placeholder={user.Birthday}
+                            />
+                        </Form.Group>
+                        <Button type="submit" onClick={handleupdate} className="mt-2">Update</Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
+    )
 }
